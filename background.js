2ce53e8e4f5eabaaee2c3d5efbad92f82fed8e2d
dimas <cartobus@gmail.com>
2014-02-11 22:44:24 +0200

    connection established

---
+++ background.js
@@ -0,0 +1,4 @@
+// chrome.runtime.onMessa ge.addListener(function(request, sender, sendResponse){
+// 	if (request.img == 'fpImg')
+// 		sendResponse('trololo')
+// });
--- fp.js
+++ fp.js
@@ -10,7 +10,14 @@ document.onmouseup = function(e){
 var popup = {
 	sel: "",
 	invoke: function(x,y){
-		body.insertAdjacentHTML('<img id="notes_float_panel" src="edit.png">', x, y)
+
+		var fp = document.createElement('img');
+		fp.id = 'notes_float_panel';
+		fp.src = chrome.extension.getURL("edit.png");
+		fp.style = 'position:absolute;'+
+				   'left:' +x+
+				   'rigth:'+y
+		document.body.appendChild(fp);
 	},
 
 	getSel: function(s){
