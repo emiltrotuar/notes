042b12b381b06d4d4aed1a182eaf90bd3cf87cf9
dimas <cartobus@gmail.com>
2014-02-12 16:43:23 +0200

    connected with main app

--- fp.js
+++ fp.js
@@ -3,12 +3,15 @@ document.onmouseup = function(e){
 	cx = e.clientX 
 	cy = e.clientY
 	selection = window.getSelection();
+	data = selection.anchorNode.data
+	if(data == undefined)
+		return;
 	popup.invoke(cx+50, cy+50);
-	popup.getSel(selection);
+	popup.getSel(data);
 }
 
 var popup = {
-	sel: "",
+	data: "",
 	invoke: function(x,y){
 
 		var fp = document.createElement('img');
@@ -16,22 +19,25 @@ var popup = {
 		fp.src = chrome.extension.getURL("edit.png");
 		fp.style = 'position:absolute;'+
 				   'left:' +x+
-				   'rigth:'+y
+				   'rigth:'+y;
+		fp.addEventListener("click", popup.sendData)
 		document.body.appendChild(fp);
 	},
 
-	getSel: function(s){
-		this.sel = s
+	getSel: function(data){
+		this.data = data;
 	},
 
 	sendData: function(){
+		var data = 'note[content]='+encodeURIComponent(popup.data)
 		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "http://mytods.com/notes", true);
+		xhr.open("POST", "http://localhost:3000/notes", true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4) {
 				var resp = JSON.parse(xhr.responseText);
 			}
 		}
-		xhr.send();
+		xhr.send(data);
 	}
 }
