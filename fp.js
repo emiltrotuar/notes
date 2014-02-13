6dbaf3f3a3285c75b05c9750f9844b566111c985
dimas <cartobus@gmail.com>
2014-02-13 16:27:34 +0200

    able to send json

--- fp.js
+++ fp.js
@@ -1,46 +1,80 @@
-
 document.onmouseup = function(e){
-	var fp = document.getElementById('notes_float_panel')
-	if(fp)
-	  document.body.removeChild(fp)
-	cx = e.clientX;
-	cy = e.clientY;
-	selection = window.getSelection();
-	data = encodeURIComponent(selection);
-	if(data == "")
-		return;
-	popup.invoke(cx+50, cy+50);
-	popup.getSel(selection);
+  var fp = document.getElementById('notes_float_panel');
+  var selection = window.getSelection().toString();
+  if( check(selection,fp) ){
+    var cx = e.clientX;
+    var cy = e.clientY;
+    popup.invoke(cx+50, cy+50);
+    popup.getSel(selection);
+  } 
+}
+
+function check(selection,fp){
+  if(selection.length && fp){
+    fp.removeEventListener("mousedown", popup.sendData);
+    document.body.removeChild(fp);
+    popup.exists = false;
+    return;
+  }
+  else if(selection.length && !fp && popup.exists){
+    popup.exists = false
+    return;
+  }
+  else if(!selection.length && fp){
+    fp.removeEventListener("mousedown", popup.sendData);
+    document.body.removeChild(fp);
+    popup.exists = false;
+    return;
+  }
+  else if(!selection.length && !fp){
+    return;
+  }
+  else 
+    return true;
 }
 
 var popup = {
-	data: "",
-	invoke: function(x,y){
+  exists: false,
+  data: "",
 
-		var fp = document.createElement('img');
-		fp.id = 'notes_float_panel';
-		fp.src = chrome.extension.getURL("edit.png");
-		fp.style = 'position:absolute;'+
-				   'left:' +x+
-				   'rigth:'+y;
-		// fp.addEventListener("click", popup.sendData);
-		document.body.appendChild(fp);
-	},
+  invoke: function(x,y){
+    var fp = document.createElement('img');
+    fp.id = 'notes_float_panel';
+    fp.src = chrome.extension.getURL("edit.png");
+    fp.style = 'position:absolute;'+
+               'left:' +x+
+               'rigth:'+y;
+    fp.addEventListener("mousedown", popup.sendData);
+    document.body.appendChild(fp);
+    popup.exists = true;
+  },
 
-	getSel: function(data){
-		this.data = data;
-	},
+  getSel: function(data){
+    this.data = data;
+  },
 
-	sendData: function(){
-		var data = 'note[content]='+encodeURIComponent(popup.data)
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "http://localhost:3000/notes", true);
-		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
-				var resp = JSON.parse(xhr.responseText);
-			}
-		}
-		xhr.send(data);
-	}
+  sendData: function(ev){
+    ev.stopPropagation();
+    var fp = document.getElementById('notes_float_panel');
+    fp.removeEventListener("mousedown", popup.sendData);
+    document.body.removeChild(fp);
+
+    var data = JSON.stringify({note: {content: popup.data}})
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/notes.json", true);
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        var resp = JSON.parse(xhr.responseText);
+        var options = {
+          type:    "basic",
+          title:   resp.created_at,
+          message: resp.content,
+          iconUrl: chrome.extension.getURL("edit.png")
+        }
+        chrome.notifications.create("", options, function(noteId){});
+      }
+    }
+    xhr.send(data);
+  }   
 }
